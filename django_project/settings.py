bb7b9bf1e3a8ae0775ee41cd28203d1a73acd289
Bulat <bibikbaev@edu.hse.ru>
2022-10-13 23:09:07 +0300

    crispy

--- django_project/settings.py
+++ django_project/settings.py
@@ -131,3 +131,10 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
